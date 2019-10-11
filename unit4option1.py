157eecc631a3d1b130651fced41f2858f01e1fcd
ariana <ariana.daney@ssfs.org>
2019-10-11 09:00:24 -0400

    worked on unit 4 option1 final project gave the user cards

--- unit4option1.py
+++ unit4option1.py
@@ -5,5 +5,45 @@
 import random
 
 
-def assign_the_user_numbers():
-    random.randint(1, 10)
+def get_cards():
+    return random.randint(1, 10)
+
+
+def print_cards(card1, card2, user_total):
+    print("you drew a", card1, "and a", card2)
+    print("your total is", user_total)
+
+
+def another_card(card3, new_total):
+    another = input("do you want another card? please enter y for yes or n for no")
+    if another == "y":
+        print("you drew a", card3, "and your total is now", new_total)
+
+
+def dealer_cards():
+    card4 = get_cards()
+    card5 = get_cards()
+    dealer_total = card4 + card5
+    print("the dealer drew a", card4, "and a", card5, "so their total is", dealer_total)
+
+
+def win_loose(new_total, user_total):
+    if new_total > 21 or user_total > 21:
+        print("your cards are more than 21, you lost so don't go to vegas")
+    else:
+        dealer_cards()
+
+
+def main():
+    card1 = get_cards()
+    card2 = get_cards()
+    user_total = card1 + card2
+    print_cards(card1, card2, user_total)
+    card3 = get_cards()
+    new_total = user_total + card3
+    another_card(card3, new_total)
+    win_loose(new_total, user_total)
+
+
+
+main()
